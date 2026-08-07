09b0b9683b839aca49e460581142d254b3193d74
Ruthik M U <rruthik355@gmail.com>
2026-08-07 21:00:30 +0530

    Time: 11 ms (46.15%), Space: 59.8 MB (94.76%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+
+    result1=[]
+    result2=[]
+
+    for (let num of set1){
+        if(!set2.has(num)){
+            result1.push(num)
+        }
+    }
+
+    for(let num of set2){
+        if(!set1.has(num)){
+            result2.push(num)
+        }
+    }
+    return [result1,result2]
+};
