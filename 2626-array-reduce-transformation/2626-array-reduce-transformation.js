4e24bc225abd925ee6fe948580dcbef9b204c635
Ruthik M U <rruthik355@gmail.com>
2026-08-21 10:24:43 +0530

    Time: 52 ms (20.92%), Space: 54.4 MB (34.9%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let result = init;
+    for(let i=0;i<nums.length;i++){
+        result = fn(result,nums[i])
+    }
+    return result
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
