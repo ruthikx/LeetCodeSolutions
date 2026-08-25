eadf66e8f95ec0bd1a36afb6fd0034e4f4184d57
Ruthik M U <rruthik355@gmail.com>
2026-08-25 15:31:35 +0530

    Time: 53 ms (37.9%), Space: 54.3 MB (28.96%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(()=>{fn(...args)},t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
