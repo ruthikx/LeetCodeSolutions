9530a1dd255bb947dd4af91de724b637ff7c579f
Ruthik M U <rruthik355@gmail.com>
2026-09-10 13:01:42 +0530

    Time: 108 ms (65.67%), Space: 70 MB (54.7%) - LeetHub

---
+++ 2724-sort-by/2724-sort-by.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b)=>fn(a)-fn(b))
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
